980110684d9edbf7ddb808ab4d0865ebf8ae2846
shotttik <shota.akhlouri@gmail.com>
2021-01-27 12:23:35 +0400

    Added manager model as user and fixed coupon status

--- carwash/models.py
+++ carwash/models.py
@@ -1,14 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 from carwash.vehichle_type_choices import VehicleTypeChoices
 from carwash.vip_status_choices import VipStatusChoices
 
+''' 
+    კუპონების გამოყენება ხდება ერჯერადად, არის რამდენიმე სტატუსის მქონე, ერთი სტატუსის შეიძლება ბევრი იყოს გამოშვებული.
+    ავტომობილს შეუძლია ერთი სახეობის კუპონით გარეცხვა ბევრჯერ.
+'''
 
-# იდეაში ერთჯერადი კუპონებია.
+
 class Coupon(models.Model):
     vip_status = models.CharField(max_length=255, choices=VipStatusChoices.choices,
-                                  default=VipStatusChoices.Bronze)
+                                  default=VipStatusChoices.Bronze,
+                                  unique=True)  # ერთი სტატუსის ორი კუპონი რომ არ შეიქმნას
     discount = models.CharField(max_length=255, verbose_name='Discount')
-    gift = models.TextField(verbose_name='Gift', blank=True)  # კუპონი არ წაიშლება შეიცვლება ფასდაკლება ან საჩუქარი.
+    gift = models.TextField(verbose_name='Gift', blank=True)
 
     def __str__(self):
         return self.vip_status
@@ -24,7 +30,9 @@ class Vehicle(models.Model):
     started = models.DateTimeField(verbose_name='Started')
     finished = models.DateTimeField(verbose_name='Finished', auto_now=True)  # გარეცხვის შემდეგ შედის ბაზაში
     plate_number = models.CharField(max_length=15)  # unique=False რათა ბევრჯერ გაირეცხოს
-    coupons = models.ForeignKey(to='carwash.Coupon', on_delete=models.PROTECT,
+    # coupons = models.ManyToManyField(to='carwash.Coupon', blank=True) ერთ გარეცხვაზე რამდენიმე კუპონის გამოყენება არშე
+    coupons = models.ForeignKey(to='carwash.Coupon',
+                                on_delete=models.PROTECT,  # კუპონი არ წაიშლება შეიცვლება ფასდაკლება ან საჩუქარი.
                                 blank=True, null=True)  # კუპონის გარეშე მანქანის გარეცხვა
     washer = models.ForeignKey(to='carwash.Washer',
                                on_delete=models.SET_NULL,  # მრეცხავის წაშლის შემთხვევაში გარეცხილი მანქანა რომ დარჩეს
@@ -56,6 +64,7 @@ class Washer(models.Model):
 
 
 class Manager(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)  # user თუ წაიშლება მენეჯერიც რომ წაიშალოს
     full_name = models.CharField(verbose_name='Full Name', max_length=255, unique=True)
     age = models.PositiveSmallIntegerField(verbose_name='Age', default=0)
     email = models.EmailField(verbose_name="E-mail", unique=True)
