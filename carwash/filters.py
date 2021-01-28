bcbf27a298900c211d391633a706825ad95f3356
shotttik <shota.akhlouri@gmail.com>
2021-01-29 00:17:39 +0400

    Added date filter, fixed order page

--- carwash/filters.py
+++ carwash/filters.py
@@ -1,8 +1,12 @@
 import django_filters
 from carwash.models import Order
+from django_filters import DateFilter
 
 
 class OrderFilter(django_filters.FilterSet):
+    start_date = DateFilter(field_name="date_ordered", lookup_expr='gte', label='Date greater than')
+    end_date = DateFilter(field_name='date_ordered', lookup_expr='lte', label='Date less than')
+
     class Meta:
         model = Order
         fields = ('type', 'plate_number', 'coupons', 'washer')
--- carwash/models.py
+++ carwash/models.py
@@ -33,8 +33,7 @@ class Order(models.Model):
     type = models.CharField(max_length=255, verbose_name="Vehicle Type", choices=VehicleTypeChoices.choices,
                             default=VehicleTypeChoices.SEDAN)
     plate_number = models.CharField(max_length=15, unique=False)  # ერთი ავტომობილი ბევრჯერ გაირეცხოს
-    started = models.DateTimeField(verbose_name='Started')
-    finished = models.DateTimeField(verbose_name='Finished', auto_now=True)  # გარეცხვის შემდეგ შედის ბაზაში
+    date_ordered = models.DateTimeField(verbose_name='Date Ordered', auto_now=True)  # გარეცხვის შემდეგ შედის ბაზაში
     coupons = models.ForeignKey(to='carwash.Coupon',
                                 on_delete=models.PROTECT,  # კუპონი არ წაიშლება შეიცვლება ფასდაკლება ან საჩუქარი.
                                 blank=True, null=True)  # კუპონის გარეშე მანქანის გარეცხვა
