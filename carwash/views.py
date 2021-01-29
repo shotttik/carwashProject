1863cda50ea7ae8c45805c231cb4af37fcdb4f74
shotttik <shota.akhlouri@gmail.com>
2021-01-29 21:00:26 +0400

    washer order count updated

--- carwash/views.py
+++ carwash/views.py
@@ -3,14 +3,19 @@ from carwash.models import Manager, Washer, Order
 
 
 def team(request):
+    customer = Washer.objects.get()
+    washer_orders = customer.orders.all()
+    orders_count = washer_orders.count()
     washers = Washer.objects.all
     managers = Manager.objects.all
     return render(request, 'team.html', {'washers': washers,
-                                         'managers': managers})
+                                         'managers': managers,
+                                         'orders_count': orders_count,
+                                         })
 
 
 def order(request):
-    orders = Order.objects.all()
+    orders = Order.objects.all().order_by('-id')
     order_count = orders.count()
     return render(request, 'order.html', {'orders': orders,
                                           'orders_count': order_count})
